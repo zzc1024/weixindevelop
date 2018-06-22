dbb29f33968f555fd281926b17c364d3b154a8bf
zzc1024 <1095395035@qq.com>
2018-06-22 22:14:23 +0800

    更新“ ； ' “

---
+++ layout/pages/index/index.js
@@ -0,0 +1,89 @@
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    product:[
+      {
+        title:'摩卡咖啡',
+        imgurl: 'http://182.61.36.176/static/img/public/15296547428710.png'
+      },
+      {
+        title: '卡布奇诺',
+        imgurl: 'http://182.61.36.176/static/img/public/15296547425587.png'
+      },
+      {
+        title: '黑咖啡',
+        imgurl: 'http://182.61.36.176/static/img/public/15296547427741.png'
+      },
+      {
+        title: '卡布奇诺',
+        imgurl: 'http://182.61.36.176/static/img/public/15296547425587.png'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
